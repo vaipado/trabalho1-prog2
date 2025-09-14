49cee455853b652775a3c89fb5b8b6f0d4503b02
Carlos <carnot.br@gmail.com>
2025-09-14 18:02:32 -0300

    Implementação do arquivo canvas.cpp

---
+++ canvas.cpp
@@ -0,0 +1,32 @@
+#include "canvas.hpp"
+
+void CriarCanvas (Canvas &tela, int largura, int altura){
+    // Código da lógica será implementado aqui
+}
+void DestruirCanvas(Canvas &tela){
+    // Código da lógica será implementado aqui
+}
+void ImprimirCanvas(const Canvas& tela){
+    // Código da lógica será implementado aqui
+}
+void DesenharPonto(Canvas& tela, int x, int y, char simbolo, char cor){
+    // Código da lógica será implementado aqui
+}
+void DesenharLinha(Canvas& tela, int x1, int y1, int x2, int y2, char simbolo, char cor){
+    // Código da lógica será implementado aqui
+}
+void DesenharRetangulo(Canvas& tela, int x, int y, int largura, int altura, char simbolo, char cor){
+    // Código da lógica será implementado aqui
+}
+void DesenharRetanguloPreenchido(Canvas& tela, int x, int y, int largura, int altura, char simbolo, char cor){
+    // Código da lógica será implementado aqui
+}
+void RedimensionarCanvas(Canvas& tela, int novaLargura, int novaAltura){
+    // Código da lógica será implementado aqui
+}
+void SobreporCanvas(Canvas& telaDestino, const Canvas& telaOrigem1, const Canvas& telaOrigem2){
+    // Código da lógica será implementado aqui
+}
+bool CompararCanvas(const Canvas& tela1, const Canvas& tela2){
+    // Código da lógica será implementado aqui
+}
