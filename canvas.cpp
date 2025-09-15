533017d9c9aa207e55cf7e42b2ccd99147e274a5
Carlos <carnot.br@gmail.com>
2025-09-14 21:55:24 -0300

    Implementação da function DestruirCanvas

--- canvas.cpp
+++ canvas.cpp
@@ -1,8 +1,10 @@
 #include "canvas.hpp"
 #include <iostream>
 
-void CriarCanvas (Canvas &tela, int largura, int altura){
-    if(largura <= 0 || altura <= 0){
+void CriarCanvas(Canvas &tela, int largura, int altura)
+{
+    if (largura <= 0 || altura <= 0)
+    {
         std::cerr << "Largura ou altura inválida" << std::endl;
         tela.largura = 0;
         tela.altura = 0;
@@ -16,41 +18,75 @@ void CriarCanvas (Canvas &tela, int largura, int altura){
 
     tela.pixels = new char *[altura];
     tela.cores = new char *[altura];
-    
-    for(int i = 0; i < altura; i++){
-        tela.pixels[i] = new char [largura];
-        tela.cores[i] = new char [largura];
 
-        for(int j = 0; j < largura; j++){
+    for (int i = 0; i < altura; i++)
+    {
+        tela.pixels[i] = new char[largura];
+        tela.cores[i] = new char[largura];
+
+        for (int j = 0; j < largura; j++)
+        {
             tela.pixels[i][j] = ' ';
-            tela.cores[i][j] = 'W'
+            tela.cores[i][j] = 'W';
         }
     }
 }
-void DestruirCanvas(Canvas &tela){
+
+void DestruirCanvas(Canvas &tela)
+{
+    if (tela.pixels != nullptr)
+    {
+        for (int i = 0; i < tela.altura; i++)
+        {
+            delete[] tela.pixels[i];
+        }
+        delete[] tela.pixels;
+        tela.pixels = nullptr;
+    }
+
+    if (tela.cores != nullptr)
+    {
+        for (int i = 0; i < tela.altura; i++)
+        {
+            delete[] tela.cores[i];
+        }
+        delete[] tela.cores;
+        tela.cores = nullptr;
+    }
+
+    tela.altura = 0;
+    tela.largura = 0;
+}
+
+void ImprimirCanvas(const Canvas &tela)
+{
     // Código da lógica será implementado aqui
 }
-void ImprimirCanvas(const Canvas& tela){
+void DesenharPonto(Canvas &tela, int x, int y, char simbolo, char cor)
+{
     // Código da lógica será implementado aqui
 }
-void DesenharPonto(Canvas& tela, int x, int y, char simbolo, char cor){
+void DesenharLinha(Canvas &tela, int x1, int y1, int x2, int y2, char simbolo, char cor)
+{
     // Código da lógica será implementado aqui
 }
-void DesenharLinha(Canvas& tela, int x1, int y1, int x2, int y2, char simbolo, char cor){
+void DesenharRetangulo(Canvas &tela, int x, int y, int largura, int altura, char simbolo, char cor)
+{
     // Código da lógica será implementado aqui
 }
-void DesenharRetangulo(Canvas& tela, int x, int y, int largura, int altura, char simbolo, char cor){
+void DesenharRetanguloPreenchido(Canvas &tela, int x, int y, int largura, int altura, char simbolo, char cor)
+{
     // Código da lógica será implementado aqui
 }
-void DesenharRetanguloPreenchido(Canvas& tela, int x, int y, int largura, int altura, char simbolo, char cor){
+void RedimensionarCanvas(Canvas &tela, int novaLargura, int novaAltura)
+{
     // Código da lógica será implementado aqui
 }
-void RedimensionarCanvas(Canvas& tela, int novaLargura, int novaAltura){
+void SobreporCanvas(Canvas &telaDestino, const Canvas &telaOrigem1, const Canvas &telaOrigem2)
+{
     // Código da lógica será implementado aqui
 }
-void SobreporCanvas(Canvas& telaDestino, const Canvas& telaOrigem1, const Canvas& telaOrigem2){
-    // Código da lógica será implementado aqui
-}
-bool CompararCanvas(const Canvas& tela1, const Canvas& tela2){
+bool CompararCanvas(const Canvas &tela1, const Canvas &tela2)
+{
     // Código da lógica será implementado aqui
 }
